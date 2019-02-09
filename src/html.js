7ef26110d5396d067410e06c4134920b024aea58
ZacharyChim <zecharyw@gmail.com>
2019-02-09 13:21:10 +0100

    remove adsense

--- src/html.js
+++ src/html.js
@@ -25,15 +25,6 @@ export default class HTML extends React.Component {
           <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
-          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <script>
-              {`
-                (adsbygoogle = window.adsbygoogle || []).push({
-                  google_ad_client: "ca-pub-7299149219813304",
-                  enable_page_level_ads: true
-                });
-              `}
-          </script>
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
