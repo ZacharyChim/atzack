4c3bb5996c66505a078bf5c263ea2117bcba46ab
ZacharyChim <zecharyw@gmail.com>
2019-03-25 16:46:53 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-98c2f2012e6e672ca0ae.js"
+    "url": "webpack-runtime-799efbed37436947105e.js"
   },
   {
     "url": "styles.b96d7caf52386aa20c91.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "809bf57b12de328c54b9aa1e49577fb4"
+    "revision": "e2805c754b5690a29bd68b0d170ac844"
   },
   {
     "url": "component---src-pages-404-js-eb8ece5940c4e51bf3d1.js"
